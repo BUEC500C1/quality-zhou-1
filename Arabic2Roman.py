423457ee9ceed421fb8ecfd29ffbc19020e71a43
Zhou <gentle_zhou_shen@outlook.com>
2020-01-24 17:22:50 -0500

    Update Arabic2Roman.py

--- Arabic2Roman.py
+++ Arabic2Roman.py
@@ -1,7 +1,12 @@
 print("Arabic 2 Roman program starts. \n")
 
 def Arab2Roman(num):
-
+    M = ["", "M", "MM", "MMM"];
+    C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    ans = M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10]
+    return ans
 
 if __name__ == '__main__':
     x = 1000
