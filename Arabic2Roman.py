65b0686a307b4947afa43d7e88351061309643cb
Zhou <gentle_zhou_shen@outlook.com>
2020-01-26 11:16:20 -0500

    Update Arabic2Roman.py

--- Arabic2Roman.py
+++ Arabic2Roman.py
@@ -5,8 +5,12 @@ def 2Roman(num):
     C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
     X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
     I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
-    ans = M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10]
-    return ans
+    
+    if num < 0:
+        return "-1"
+    else:
+        ans = M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10]
+        return ans
 
 #if __name__ == '__main__':
 #    x = 1000
