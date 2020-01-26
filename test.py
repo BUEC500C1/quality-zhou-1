84113ece42fb736809afdbc8c318dd6e63e717f7
Zhou <gentle_zhou_shen@outlook.com>
2020-01-26 11:49:48 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 # test for roman 2 Intgeer converter
 
 import pytest
-import Arabic2Roman
+import ArabicToRoman
 
 def testConverter():
   arabic = [1, 10, 101, 1999, -10]
   roman = ["I", "X", "CI", "MCMXCIX", "-1"]
   
   for a,r in zip(arabic,roman):
-	  assert Arabic2Roman.toRoman(a) == r
+	  assert ArabicToRoman.toRoman(a) == r
 
     
 # if __name__ == "__main__":
