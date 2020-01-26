9eba349a4a654b5b011054709808ce0475da51c2
Zhou <gentle_zhou_shen@outlook.com>
2020-01-26 11:15:08 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,13 @@
 import pytest
 import Arabic2Roman
 
+def testConverter():
+  arabic = [1, 10, 101, 1999]
+  roman = ["I", "X", "CI", "MCMXCIX"]
+  
+  for a,r in zip(arabic,roman):
+	  assert Arabic2Roman.2Roman(a) == r
 
+    
+# if __name__ == "__main__":
+# 	testConverter() 
