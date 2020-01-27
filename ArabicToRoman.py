b18bcf2ddda1967574943b29bbb0bab33e70470c
Zhou <gentle_zhou_shen@outlook.com>
2020-01-27 16:50:31 -0500

    Update ArabicToRoman.py

--- ArabicToRoman.py
+++ ArabicToRoman.py
@@ -1,10 +1,10 @@
 # print("Arabic to Roman program starts. \n")
 
-strs = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
-nums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]        
-ret = ""  
-
 def toRoman(num):
+    strs = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+    nums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]        
+    ret = ""  
+    
     if num < 0:
         return "-1"
     for i, j in enumerate(nums):
