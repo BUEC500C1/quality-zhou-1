d04350d4df74531a4c3ef7ac49f52cb5fb2fc924
Zhou <gentle_zhou_shen@outlook.com>
2020-01-27 19:27:25 -0500

    Update test_ArabicToRoman.py

--- test_ArabicToRoman.py
+++ test_ArabicToRoman.py
@@ -4,8 +4,8 @@ import pytest
 import ArabicToRoman
 
 def test_Converter():
-  arabic = [1, 10, 101, 1999, -10]
-  roman = ["I", "X", "CI", "MCMXCIX", "-1"]
+  arabic = [1, 10, 101, 1999, -10, 2865, 901, -1]
+  roman = ["I", "X", "CI", "MCMXCIX", "-1", "MMDCCCLXV", "CMI", "-1"]
   
   for a,r in zip(arabic,roman):
 	  assert ArabicToRoman.toRoman(a) == r
